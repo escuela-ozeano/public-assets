a3a005114692013ee3d6634c5a54d08457b7f2fa
Helder Manuel Torres Vieira <helder.torres@innki.tech>
2023-04-25 17:34:22 +0200

    chore: :rocket: added thrivecart and wordpress scripts

---
+++ scripts/wordpress/thrivecart-loader.js
@@ -0,0 +1,43 @@
+// ---------------------------------------------------------------- //
+// Copyright: Innki Tech 2023. All Rights Reserved                  //
+// Mantainer: Helder Torres <helder.torres@innki.tech>              //
+// This file is licensed under the MPL 2.0 license                  //
+// License text available at https://www.mozilla.org/en-US/MPL/2.0  //
+// ---------------------------------------------------------------- //
+
+// Settings
+const queryParams = new URLSearchParams(window.location.search);
+const couponCodeValue = queryParams.get(couponCodeParam);
+const payFromSpainSelector = document.querySelector(`#${payFromSpainParam}`);
+const payTaxExemptionSelector = document.querySelector(`#${payTaxExemptionParam}`);
+
+// Add Thrivecart Params
+if (couponCodeValue !== null) thrivecartObj.queryParams["coupon"] = couponCodeValue;
+if (couponCodeCacheParam !== null) thrivecartObj.queryParams["coupon_cache"] = couponCodeCacheParam === 'true';
+thrivecartObj.queryParams["passthrough[utm_source]"] = queryParams.get("utm_source");
+thrivecartObj.queryParams["passthrough[utm_campaign]"] = queryParams.get("utm_campaign");
+thrivecartObj.queryParams["passthrough[utm_content]"] = queryParams.get("utm_content");
+thrivecartObj.queryParams["passthrough[utm_medium]"] = queryParams.get("utm_medium");
+thrivecartObj.queryParams["passthrough[utm_term]"] = queryParams.get("utm_term");
+
+// Store Params
+storeParam(payProcessorParam, "mx");
+
+// Store Query Params
+storeQueryParam(queryParams, payFromSpainParam);
+storeQueryParam(queryParams, payTaxExemptionParam);
+
+// Load Params
+loadParam(payFromSpainParam, payFromSpainSelector, "select");
+loadParam(payTaxExemptionParam, payTaxExemptionSelector, "select");
+
+// Remove Query Params
+window.removeQueryParams();
+
+// Add Event Listeners
+payFromSpainSelector.addEventListener("change", evt => window.storeParam(evt.target.id, evt.target.value));
+payFromSpainSelector.addEventListener("change", evt => window.loadParam(payTaxExemptionParam, payTaxExemptionSelector, "select"));
+payTaxExemptionSelector.addEventListener("change", evt => window.storeParam(evt.target.id, evt.target.value));
+payFromSpainSelector.addEventListener("change", () => window.toggleThrivecartCheckout(thrivecartObj, payProcessorParam, payFromSpainParam, payTaxExemptionParam));
+payTaxExemptionSelector.addEventListener("change", () => window.toggleThrivecartCheckout(thrivecartObj, payProcessorParam, payFromSpainParam, payTaxExemptionParam));
+window.addEventListener("load", () => window.toggleThrivecartCheckout(thrivecartObj, payProcessorParam, payFromSpainParam, payTaxExemptionParam));
